45fddd0f4d20b41fda0f9680ddd69f3f1464a2f5
Automedon <42299107+Automedon@users.noreply.github.com>
2021-07-25 12:03:43 +0700

    User Model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
-import {Module} from '@nestjs/common'
-import { AppController } from "./app.contoller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { User } from './users/users.model';
 
 @Module({
-  controllers:[AppController],
-  providers:[AppService]
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
 })
-export class AppModule {
-
-}
+export class AppModule {}
